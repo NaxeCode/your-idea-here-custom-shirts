729f0ad2aa200019869c0c8f680ee9290b15a0ff
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-21 10:13:22 -0800

    Updated Footer to include my credits

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import Logo from "../../img/yourideaherelogo-1.png"
-import {navigate} from 'gatsby'; //import navigate from gatsby
+import React from 'react'
+import Logo from '../../img/yourideaherelogo-1.png'
+import { navigate } from 'gatsby' //import navigate from gatsby
 
-import "../style/Main.scss"
+import '../style/Main.scss'
 
 export default function Home() {
-    return (
-        <div className="Main">
-            <mainSite className="Content">
-                <img className="Logo" src={Logo} alt="Shirt that reads 'Your Idea Here'" />
-                <h1 className="Title">Your Idea Here</h1>
-                <h3 className="Text">Create Your Own Custom Shirt</h3>
-                <button className="Button" onClick={() => editPage()}><p className="ButtonTxt">Shop Now</p></button>
-            </mainSite>
-        </div>
-    );
+	return (
+		<div className="Main">
+			<mainSite className="Content">
+				<img src={Logo} alt="Shirt that reads 'Your Idea Here'" />
+				<h1 className="Title">Your Idea Here</h1>
+				<h3 className="Text">Create Your Own Custom Shirt</h3>
+				<button className="Button" onClick={() => editPage()}>
+					<p className="ButtonTxt">Shop Now</p>
+				</button>
+			</mainSite>
+		</div>
+	)
 }
 
 function editPage(index) {
-    navigate('/shop'); //navigate to edit page
+	navigate('/shop') //navigate to edit page
 }
