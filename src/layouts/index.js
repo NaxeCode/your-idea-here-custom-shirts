5dce19261d90ddb94548da8b4f8755c4e5e71625
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-21 09:42:52 -0800

    Updated React Helmet metadata title to Your Idea Here

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import '../style/index.scss'
 
 const Layout = ({ children, site }) => (
   <div>
-    <Helmet title="Snipcart + DatoCMS + GatsbyJS Example" />
+    <Helmet title="Your Idea Here" />
     <div className="Container">
       <div className="Header">
         <div className="Wrap" >
